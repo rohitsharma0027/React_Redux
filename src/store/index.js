bb16c5d8220b2b4505f2ccd64a62db1ab11bec16
developer.rohitsharmaj7@gmail.com <rohitsharmaj7@gmail.com>
2021-06-15 08:23:27 +0530

    refactored some code

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,35 @@
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import { createSlice,configureStore     } from '@reduxjs/toolkit';   //APPROACH USING REDUX TOOLKIT 
 
+// const initialAuthState = { isAuthenticated: false}
+
+// const authSlice = createSlice({
+//     name:'authentication',
+//     initialState:initialAuthState ,
+//     reducers:{
+//         login(state){
+//             state.isAuthenticated = true;
+//         },
+//         logout(state){
+//             state.isAuthenticated=false;
+//         }
+//     }
+// });
+
+// export const authAction = authSlice.actions;
+// const store = configureStore({reducer:{auth:authSlice.reducer}})
+// End of redux toolkit approach
+
+
+// APPROACH 1 CODE BLOCK STARTING
 // initial state object
-const initialState = { counter:0, showCounter:true }
+const initialCounterState = { counter:0, showCounter:true }
 
-// UNique indentifier to keep application simple while the app grows
+// Unique indentifier to keep application simple while the app grows
 // export const INCREMENT ='increment';
 
 // Step 2: Create the reducer function
-const counterReducer = (state = initialState,action) => {
+const counterReducer = (state = initialCounterState,action) => {
     if(action.type === 'increment')
     {
         return {
@@ -43,9 +65,9 @@ const counterReducer = (state = initialState,action) => {
     return state
 };
 
-
 // Step 1: create store 
 // Step 3: Pass the reducer function to the store
 const store = createStore(counterReducer)
+// APPROACH 1 CODE BLOCK ENDING
 
 export default store;
